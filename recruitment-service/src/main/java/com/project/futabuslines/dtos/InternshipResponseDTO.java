3d43bdc3248dbcfbb63af21c2fbe4910cd07fc11
Tai-brother <138606172+Tai-brother@users.noreply.github.com>
2025-09-12 20:24:26 +0700

    add dtos

---
+++ recruitment-service/src/main/java/com/project/futabuslines/dtos/InternshipResponseDTO.java
@@ -0,0 +1,42 @@
+package com.project.futabuslines.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.project.futabuslines.models.ProgramStatus;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+public class InternshipResponseDTO {
+    private Long id;
+
+    private String title;
+
+    private String department;
+
+    private int positions;
+
+    private int applications;
+
+    private ProgramStatus status;
+    @JsonProperty("start_date")
+
+    private LocalDate startDate;
+    @JsonProperty("end_date")
+
+    private LocalDate endDate;
+
+    @JsonProperty("created_by")
+    private Long createdBy;
+    // dữ liệu user từ UserService
+    @JsonProperty("created_by_name")
+    private String createdByName;
+    @JsonProperty("created_by_email")
+    private String createdByEmail;
+
+    private List<String> requirements;
+    private List<String> benefits;
+}
---
+++ recruitment-service/src/main/java/com/project/futabuslines/dtos/RequirementDTO.java
@@ -0,0 +1,8 @@
+package com.project.futabuslines.dtos;
+
+import lombok.Data;
+
+@Data
+public class RequirementDTO {
+    private String description;
+}
