a12d7105a93b2a647d692090961f9d09346321fd
HuyNgodev123 <ngoh0839@gmail.com>
2025-09-15 14:30:10 +0700

    update Recruitment-service

---
+++ recruitment-service (1)/src/main/java/com/project/futabuslines/repositories/BenefitRepository.java
@@ -0,0 +1,8 @@
+package com.project.futabuslines.repositories;
+
+import com.project.futabuslines.models.Benefit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BenefitRepository extends JpaRepository<Benefit, Long> {
+}
+
