bf5d5f33c318cf5c30ef22debe652f0bcd7038bc
Quoc Huy <154371663+QuocHuyLearningCode@users.noreply.github.com>
2025-09-15 09:09:55 +0700

    Add files via upload

---
+++ mail-service/src/main/java/com/project/chat/producers/NotificationProducer.java
@@ -0,0 +1,34 @@
+package com.project.chat.producers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendEmail(String to, String subject, String content) {
+        String payload = """
+                {
+                  "to": "%s",
+                  "subject": "%s",
+                  "content": "%s"
+                }
+                """.formatted(to, subject, content);
+
+        kafkaTemplate.send("notification-email", payload);
+    }
+
+    public void sendSms(String to, String content) {
+        String payload = """
+                {
+                  "to": "%s",
+                  "content": "%s"
+                }
+                """.formatted(to, content);
+
+        kafkaTemplate.send("notification-sms", payload);
+    }
+}
